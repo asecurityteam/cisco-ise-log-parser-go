a2abe96e00e32cbe20ea84a84c03d415df0edd78
aalcantar <aalcantar@atlassian.com>
2022-03-08 15:26:50 -0600

    remove comment

--- messageparser.go
+++ messageparser.go
@@ -694,7 +694,7 @@ func parseEndpointProperty(logMessage *LogMessage, key string, value string) err
 		"textEncodedORAddress": parseDisregard, // Contents differ and cannot use its parse func. Information is not valuable in 80002 event so ignore
 		"#015":                 parseDisregard,
 		"cisco-av-pair":        parseCiscoAVPair,
-		"device-type":          parseDisregard, // Different type than LogMessage's DeviceType. Since it's only populated for individually owned assets (data we can find in TAG project), we can ignore
+		"device-type":          parseDisregard, // Different type than LogMessage's DeviceType, so ignore
 		"Response":             parseResponse,
 	}
 
